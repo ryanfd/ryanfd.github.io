b4446a8df94e59cb3db581fc069eb3dc2ac41b14
ryanfd <rdonnelly98@gmail.com>
2024-09-27 13:57:38 -0700

    Continued progress of custom layout group page

---
+++ js/definitions.js
@@ -0,0 +1,30 @@
+function leave() {
+    $(".sum-remainder img.sum-img").animate({opacity: 0}, {duration: 200, queue: false});
+    $(".definition").animate({left: 0}, {duration: 200, queue: false});
+    $(".definition").css("display", "block");
+}
+
+function hide() {
+    $(".definition").css("display", "none");
+}
+
+function reset() {
+    $(".definition").animate({left: "150%"}, {duration: 200, queue: false});
+    $(".sum-remainder img.sum-img").animate({opacity: 1}, {duration: 200, queue: false});
+    setTimeout(hide, 200);
+}
+
+// reset to starting display
+$(".exit").click(function() {
+    setTimeout(reset, 0);
+});
+
+$("#dirtying").click(function() {
+    $("h4.schooled-sum-definition").css("opacity", "0");
+    $("p.schooled-sum-definition").css("opacity", "0");
+    $("h4.schooled-sum-definition").text("Dirtying");
+    $("p.schooled-sum-definition").text("A process where Unity marks an object that needs to be redrawn/recalculated for the next frame.");
+    $("h4.schooled-sum-definition").animate({opacity: 1}, 200);
+    $("p.schooled-sum-definition").animate({opacity: 1}, 200);
+    setTimeout(leave, 0);
+});
