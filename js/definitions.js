b0cea5db94cfc8acdce90ca45c99ff714c2455ff
ryanfd <rdonnelly98@gmail.com>
2024-09-29 15:34:58 -0700

    First part of body section

--- js/definitions.js
+++ js/definitions.js
@@ -1,11 +1,20 @@
-function leave() {
+function leave_sum() {
     $(".sum-remainder img.sum-img").animate({opacity: 0}, {duration: 200, queue: false});
     $(".definition").animate({left: 0}, {duration: 200, queue: false});
     $(".definition").css("display", "block");
 }
+function leave_body1() {
+    $(".sum-remainder img.sum-img-body").animate({opacity: 0}, {duration: 200, queue: false});
+    $(".definition-body").animate({left: 0}, {duration: 200, queue: false});
+    $(".definition-body").css("display", "block");
+}
 
 function hide() {
     $(".definition").css("display", "none");
+}
+
+function hide_body() {
+    $(".definition-body").css("display", "none");
 }
 
 function reset() {
@@ -14,9 +23,19 @@ function reset() {
     setTimeout(hide, 200);
 }
 
+function reset_body() {
+    $(".definition-body").animate({left: "150%"}, {duration: 200, queue: false});
+    $(".sum-remainder img.sum-img-body").animate({opacity: 1}, {duration: 200, queue: false});
+    setTimeout(hide_body, 200);
+}
+
 // reset to starting display
 $(".exit").click(function() {
     setTimeout(reset, 0);
+});
+
+$(".exit-body").click(function() {
+    setTimeout(reset_body, 0);
 });
 
 $("#dirtying").click(function() {
@@ -26,7 +45,7 @@ $("#dirtying").click(function() {
     $("p.schooled-sum-definition").text("A process where Unity marks an object that needs to be redrawn/recalculated for the next frame.");
     $("h4.schooled-sum-definition").animate({opacity: 1}, 200);
     $("p.schooled-sum-definition").animate({opacity: 1}, 200);
-    setTimeout(leave, 0);
+    setTimeout(leave_sum, 0);
 });
 
 $("#horizontal-layout-group").click(function() {
@@ -36,7 +55,7 @@ $("#horizontal-layout-group").click(function() {
     $("p.schooled-sum-definition").text("Organize elements horizontally.");
     $("h4.schooled-sum-definition").animate({opacity: 1}, 200);
     $("p.schooled-sum-definition").animate({opacity: 1}, 200);
-    setTimeout(leave, 0);
+    setTimeout(leave_sum, 0);
 });
 
 $("#grid-layout-group").click(function() {
@@ -46,7 +65,7 @@ $("#grid-layout-group").click(function() {
     $("p.schooled-sum-definition").text("Organize items into a grid.");
     $("h4.schooled-sum-definition").animate({opacity: 1}, 200);
     $("p.schooled-sum-definition").animate({opacity: 1}, 200);
-    setTimeout(leave, 0);
+    setTimeout(leave_sum, 0);
 });
 
 $("#vertical-layout-group").click(function() {
@@ -56,5 +75,15 @@ $("#vertical-layout-group").click(function() {
     $("p.schooled-sum-definition").text("Organize objects vertically.");
     $("h4.schooled-sum-definition").animate({opacity: 1}, 200);
     $("p.schooled-sum-definition").animate({opacity: 1}, 200);
-    setTimeout(leave, 0);
+    setTimeout(leave_sum, 0);
+});
+
+$("#class-diagram").click(function() {
+    $("h4.layout-group-sum-definition").css("opacity", "0");
+    $("p.layout-group-sum-definition").css("opacity", "0");
+    $("h4.layout-group-sum-definition").text("Class Diagram");
+    $("p.layout-group-sum-definition").text("A diagram that describes the structure of a system using the system's classes, attributes, methods, and the relationships among objects.");
+    $("h4.layout-group-sum-definition").animate({opacity: 1}, 200);
+    $("p.layout-group-sum-definition").animate({opacity: 1}, 200);
+    setTimeout(leave_body1, 0);
 });
